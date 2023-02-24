8fc1dc011e2318e99e6243cc6b0ee8e2061e25bd
FacundoCretton <112477366+FacundoCretton@users.noreply.github.com>
2023-02-24 13:43:40 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
-const products = document.querySelector('.product-container');
+const products = document.querySelector(".product-container");
 const botonesCategorias = document.querySelectorAll(".boton-categoria");
 const tituloPrincipal = document.querySelector("#titulo-principal");
-const categoriaDefault = 'todos';
 const categoriaList = document.querySelector(".bts-fil")
 const btnLoad = document.querySelector(".btn-load");
-const flipIcons = document.querySelectorAll('.flip-icon');
-const card = document.querySelector('.card');
+const flipIcons = document.querySelectorAll(".flip-icon");
+const card = document.querySelector(".card");
+const header=document.querySelector("header")
+const toggleThemeButton = document.getElementById("toggle-theme");
+const bodyElement = document.querySelector("body");
+const sunIcon = document.querySelector("#sun-icon");
+const moonIcon = document.querySelector("#moon-icon");
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+
+
 
 const saveLocalStorage = (cartList) => {
 	localStorage.setItem("cart", JSON.stringify(cartList));
@@ -118,21 +125,78 @@ const flipCard = () => {
 
 
 
-const applyFilter = (e) => {
-	if (!e.target.classList.contains("boton-categoria")) {
-		return;
-	} else {
-		changeFilterState(e);
-	}
-	if (!e.target.dataset.categoria) {
-		products.innerHTML = "";
-		renderProducts();
-	} else {
-		renderProducts(0, e.target.dataset.categoria);
-		productsController.nextProductsIndex = 1;
-	}
-  flipCard()
+const applyFilters = (e) => {
+  e.preventDefault();
+  e.stopPropagation(); // Agregar esta línea para evitar la propagación del evento
+  if (!e.target.classList.contains("boton-categoria")) {
+    return;
+  } else {
+    changeFilterState(e);
+  }
+
+  if (!e.target.dataset.categoria) {
+    products.innerHTML = "";
+    renderProducts();
+  } else {
+    renderProducts(0, e.target.dataset.categoria);
+    productsController.nextProductsIndex = 1;
+  }
+
+  flipCard();
+
+  // Seleccionar todos los enlaces del submenu con la clase "filtro-link"
+  const filtroLinks = document.querySelectorAll(".filtro-link");
+
+  // Agregar un event listener a cada enlace del submenu
+  filtroLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+
+      const filtro = e.target.dataset.filtro;
+      switch (filtro) {
+        case "mayor-precio":
+          filterByPrice("mayor", productsData);
+          break;
+        case "menor-precio":
+          filterByPrice("menor", productsData);
+          break;
+        case "mayor-duracion":
+          filterByDuration("mayor", productsData);
+          break;
+        case "menor-duracion":
+          filterByDuration("menor", productsData);
+          break;
+        default:
+          break;
+      }
+    });
+  });
 };
+
+
+// Función para filtrar productos por precio
+const filterByPrice = (orden, products) => {
+  products.sort((a, b) => {
+    if (orden === "mayor") {
+      return b.precio - a.precio;
+    } else if (orden === "menor") {
+      return a.precio - b.precio;
+    }
+  });
+  renderProducts(0, null, products);
+};
+
+
+const filterByDuration = (orden, products) => {
+  products.sort((a, b) => {
+    if (orden === "mayor") {
+      return b.duration - a.duration;
+    } else if (orden === "menor") {
+      return a.duration - b.duration;
+    }
+  });
+  renderProducts(0, null, products);
+};
+
 
 const isLastIndexOf = () => {
 	return (
@@ -149,11 +213,46 @@ const showMoreProducts = () => {
   flipCard()
 };
 
+// ------Navbar-scroll-----
+const toggleHeaderBackground =() => {
+  var header = document.querySelector("header");
+  header.classList.toggle("scrolled", window.scrollY > 0);
+}
+
+// ------Modo oscuro--------
+toggleTheme = () => {
+  bodyElement.classList.toggle("dark");
+  
+  if (bodyElement.classList.contains("dark")) {
+    sunIcon.style.display = "block";
+    moonIcon.style.display = "none";
+    header.classList.add("dark-mode");
+    toggleHeaderBackground(".dark header.scrolled");
+    localStorage.setItem("modo", "dark")
+  } else {
+    sunIcon.style.display = "none";
+    moonIcon.style.display = "block";
+    header.classList.remove("dark-mode");
+    toggleHeaderBackground("header.scrolled");
+    localStorage.setItem("modo", "light");
+
+  }
+}
+
+
+
+
 const init =()=>{
   renderProducts()
-  categoriaList.addEventListener("click", applyFilter);
+  categoriaList.addEventListener("click", applyFilters);
 	btnLoad.addEventListener("click", showMoreProducts);
   window.addEventListener('load', flipCard);
+  toggleThemeButton.addEventListener('click', toggleTheme);
+  window.addEventListener("scroll", toggleHeaderBackground);
+  const mode = localStorage.getItem("mode");
+  if (mode === "dark") {
+    toggleTheme();
+  }
 }
 init();
 
